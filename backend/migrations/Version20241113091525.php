5b26c2c015e473283d132ec0476184ec85477798
Mathis Krizmanic <mathis.krizmanic@gmail.com>
2024-11-13 11:18:46 +0100

    new migration for expected_duration table

---
+++ backend/migrations/Version20241113091525.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class create_expected_duration_table extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create a table for the duration of the course';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('Expected_Duration');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('subject_id', 'integer');
+        $table->addColumn('course_type_id', 'integer');
+        $table->addForeignKeyConstraint('Subject', ['subject_id'], ['id'], ['onDelete' => 'CASCADE']);
+        $table->addForeignKeyConstraint('CourseType', ['course_type_id'], ['id'], ['onDelete' => 'CASCADE']);
+        $table->addColumn('expected_duration','float');
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('Expected_Duration');
+    }
+}
