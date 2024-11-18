5d40a85606a2b6c9d511ca991c62f37afad7aac7
Tom <tom.charpentier@etu.unilim.fr>
2024-11-18 10:44:19 +0100

    Refacto Pre edit css

--- app/src/components/MainGridEdit/PreGridEdit.js
+++ app/src/components/MainGridEdit/PreGridEdit.js
@@ -14,23 +14,28 @@ const PreGridEdit = () => {
       <Header />
 
       <div className="flex flex-col items-center justify-center flex-1 space-y-10 py-10">
-        
         <div className="flex space-x-8">
+
+          {/* Champ de selection du Cursus */}
           <div className="w-64">
-          <label className="pl-4 translate-y-9 z-10 block mb-2 text-sm text-white">Cursus :</label>
-            <select
-              className="w-full min-w-40 h-20 p-3 text-white font-bold bg-primary border border-gray-300 rounded-lg shadow-sm focus:outline-none">
-              <option value="A1">A1</option>
-              <option value="A2">A2</option>
-              <option value="A3">A3 Initial</option>
-              <option value="A3-apprentice">A3 Alternant</option>
-            </select>
+            <label className="pl-4 translate-y-12 z-10 block mb-2 text-xl text-white">Cursus :</label>
+              <select
+                className="w-full min-w-48 h-28 p-3 text-white text-2xl font-bold 
+                  bg-primary border hover:bg-primaryshade border-white rounded-lg shadow-sm focus:outline-none">
+                <option value="A1">A1</option>
+                <option value="A2">A2</option>
+                <option value="A3">A3 Initial</option>
+                <option value="A3-apprentice">A3 Alternant</option>
+              </select>
           </div>
+          
         </div>
 
         <button
           type="button"
-          className="w-1/5 min-w-40 px-6 py-3 text-white bg-primary rounded-full shadow-md hover:bg-primaryshade focus:bg-primarytint focus:outline-none"
+          className="w-1/5 min-w-40 px-6 py-3 text-white text-xl 
+            bg-primary rounded-full shadow-md hover:bg-primaryshade focus:bg-primarytint 
+            focus:outline-none border border-white"
           onClick={goToPreviEdit}>
           Confirmer
         </button>
