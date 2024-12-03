f50d590cf55c7f0827fde617710168bca298b269
Tom <tom.charpentier@etu.unilim.fr>
2024-12-03 15:00:25 +0100

    Added loading screen

--- app/src/components/MainGridEdit/MainGrid.js
+++ app/src/components/MainGridEdit/MainGrid.js
@@ -21,6 +21,20 @@ const MainGrid = ({ curriculum }) => {
   const [selectedDuration, setSelectedDuration] = useState(1.0);
   const [currentSubjectIndex, setCurrentSubjectIndex] = useState(0);
 
+  const [isLoading, setLoading] = useState(true);
+  const [isGroupLoading, setIsGroupLoading] = useState(true);
+  const [isSemesterLoading, setIsSemesterLoading] = useState(true);
+  const [isSubjectLoading, setIsSubjectLoading] = useState(true);
+  const [isCourseTypeLoading, setIsCourseTypeLoading] = useState(true);
+
+
+  useEffect(() => {
+    if (!isGroupLoading && !isSemesterLoading && !isSubjectLoading && !isCourseTypeLoading) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  }, [isGroupLoading, isSemesterLoading, isSubjectLoading, isCourseTypeLoading]);
 
   {/* Gestion des SEMESTRES et MATIERES -------------------------------------------- */}
   useEffect(() => {
@@ -43,6 +57,9 @@ const MainGrid = ({ curriculum }) => {
         console.error(error);
         alert("Impossible de charger les semestres.");
       }
+      finally {
+        setIsSemesterLoading(false);
+      }
     };
 
     fetchSemesters();
@@ -63,6 +80,9 @@ const MainGrid = ({ curriculum }) => {
     } catch (error) {
       console.error(error);
       alert("Impossible de charger les matières.");
+    }
+    finally {
+      setIsSubjectLoading(false);
     }
   };
 
@@ -121,6 +141,9 @@ const MainGrid = ({ curriculum }) => {
       } catch (error) {
         console.error(error);
         alert("Impossible de charger les types de cours.");
+      }
+      finally {
+        setIsCourseTypeLoading(false);
       }
     };
 
@@ -252,6 +275,9 @@ const MainGrid = ({ curriculum }) => {
         console.error(error);
         alert("Impossible de charger les groupes.");
       }
+      finally {
+        setIsGroupLoading(false);
+      }
     };
 
     fetchGroups();
@@ -311,137 +337,150 @@ const MainGrid = ({ curriculum }) => {
 
   return (
     <div className="min-h-screen py-10">
-      <div className="flex items-center justify-start gap-5 h-20 px-10">
-        <div className="absolute top-6">
-        <ControlPanel
-            groups={groups}
-            selectedRow={selectedRow}
-            setSelectedRow={setSelectedRow}
-            selectedCol={selectedCol}
-            setSelectedCol={setSelectedCol}
-            courseTypes={courseTypes}
-            setCourseTypes={setCourseTypes}
-            updateCoursesForRemovedType={updateCoursesForRemovedType}
-            selectedCourseType={selectedCourseType}
-            setSelectedCourseType={setSelectedCourseType}
-            selectedTeacher={selectedTeacher}
-            setSelectedTeacher={setSelectedTeacher}
-            selectedDuration={selectedDuration}
-            setSelectedDuration={setSelectedDuration}
-            addItem={addItem}
-            addGroups={addGroups}
-            deleteGroups={deleteGroups}
-          />
+      {isLoading ? (
+      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-5rem)] space-y-10">
+        <div className="flex flex-col items-center bg-black bg-opacity-75 p-10 rounded-lg">
+          <div className="spinner"></div>
+          <div className="text-white text-3xl font-bold mt-4">
+            Chargement des données...
+          </div>
         </div>
-
-        {/* Choix du semestre selon le curriculum */}
-        <select
-          className="w-fit min-w-28 max-w-60 h-10 mt-2 ml-24 px-2 before:px-4 py-2 default-select rounded-full font-normal"
-          value={selectedSemester?.id || ""}
-          onChange={handleSemesterChange}>
-          <option value="" disabled>
-            Choisir un semestre
-          </option>
-          {availableSemesters.map((semester) => (
-            <option key={semester.id} value={semester.id}>
-              {semester.name}
-            </option>
-          ))}
-        </select>
-
-        {/* Choix des matières selon le semestre */}
-        <select
-          className="w-fit min-w-28 max-w-60 h-10 mt-2 px-2 before:px-4 py-2 default-select rounded-full font-normal"
-          value={selectedSubject?.id || ""}
-          onChange={handleSubjectChange}>
-          <option value="" disabled>
-            Choisir une matière
-          </option>
-          {availableSubjects.map((subject) => (
-            <option key={subject.id} value={subject.id}>
-              {subject.name}
-            </option>
-          ))}
-        </select>
-
-        {/* Bouton suivant */}
-        <button
-          onClick={() => {
-            const nextSubjectIndex =
-              availableSubjects.findIndex((s) => s.id === selectedSubject?.id) +
-              1;
-            if (nextSubjectIndex < availableSubjects.length) {
-              const nextSubject = availableSubjects[nextSubjectIndex];
-              setSelectedSubject(nextSubject);
-              setCurrentCourses(nextSubject.courses || []);
-            }
-          }}
-          disabled={
-            !selectedSubject ||
-            availableSubjects.indexOf(selectedSubject) >=
-              availableSubjects.length - 1
-          }
-          className={`flex w-48 h-10 mt-2 items-center px-4 py-2 text-white bg-primary rounded-full
-            shadow-md hover:bg-primaryshade focus:bg-primarytint border border-white focus:outline-none
-            ${
-              !selectedSubject ||
-              availableSubjects.indexOf(selectedSubject) >=
-                availableSubjects.length - 1
-                ? "bg-primaryshade cursor-not-allowed"
-                : ""
-            }`}>
-          Passer au suivant
-          <img
-            src="/images/right-arrow.svg"
-            alt="Right Arrow"
-            className="ml-2 w-4 h-4"/>
-        </button>
       </div>
-
-      {groups.length === 0 ? (
-        <div className="flex items-center justify-center w-full">
-          <div className="w-1/2 text-center text-primary mt-10 text-lg font-bold p-2 bg-white rounded-full">
-            Il n'y a pas de groupes, veuillez en ajouter pour consulter le tableau.
-          </div>
-        </div>
       ) : (
-        <DndProvider backend={HTML5Backend}>
-          <div className="ml-36 rounded-lg overflow-auto max-h-[75vh] min-h-[25rem] max-w-[85vw] -z-10">
-            <div
-              className="grid"
-              style={{
-                gridTemplateColumns: `40px repeat(${groupList.length}, minmax(5rem, 1fr))`,
-              }}>
-              <div className="w-10 h-6"></div>
-              {groupList.map((groupName, colIndex) => (
-                <div
-                  key={`col-label-${colIndex}`}
-                  className="w-full h-6 bg-gray-200 flex items-center justify-center text-black text-sm font-bold">
-                  {groupName}
-                </div>
-              ))}
-              {Array.from({ length: GRID_ROW_LENGTH }).map((_, rowIndex) => (
-                <React.Fragment key={`row-${rowIndex}`}>
-                  <div className="h-20 w-10 bg-gray-200 flex items-center justify-center text-black text-sm font-bold">
-                    S{rowIndex + 1}
-                  </div>
-                  {groupList.map((_, colIndex) => {
-                    const positionKey = `${rowIndex}-${colIndex}`;
-                    const cellItems = items[positionKey] || [];
-                    return (
-                      <Node
-                        key={positionKey}
-                        positionKey={positionKey}
-                        items={cellItems}
-                        moveItem={moveItem}
-                      />
-                    );
-                  })}
-                </React.Fragment>
-              ))}
+        <>
+          <div className="flex items-center justify-start gap-5 h-20 px-10">
+            <div className="absolute top-6">
+            <ControlPanel
+                groups={groups}
+                selectedRow={selectedRow}
+                setSelectedRow={setSelectedRow}
+                selectedCol={selectedCol}
+                setSelectedCol={setSelectedCol}
+                courseTypes={courseTypes}
+                setCourseTypes={setCourseTypes}
+                updateCoursesForRemovedType={updateCoursesForRemovedType}
+                selectedCourseType={selectedCourseType}
+                setSelectedCourseType={setSelectedCourseType}
+                selectedTeacher={selectedTeacher}
+                setSelectedTeacher={setSelectedTeacher}
+                selectedDuration={selectedDuration}
+                setSelectedDuration={setSelectedDuration}
+                addItem={addItem}
+                addGroups={addGroups}
+                deleteGroups={deleteGroups}
+              />
             </div>
+
+            {/* Choix du semestre selon le curriculum */}
+            <select
+              className="w-fit min-w-28 max-w-60 h-10 mt-2 ml-24 px-2 before:px-4 py-2 default-select rounded-full font-normal"
+              value={selectedSemester?.id || ""}
+              onChange={handleSemesterChange}>
+              <option value="" disabled>
+                Choisir un semestre
+              </option>
+              {availableSemesters.map((semester) => (
+                <option key={semester.id} value={semester.id}>
+                  {semester.name}
+                </option>
+              ))}
+            </select>
+
+            {/* Choix des matières selon le semestre */}
+            <select
+              className="w-fit min-w-28 max-w-60 h-10 mt-2 px-2 before:px-4 py-2 default-select rounded-full font-normal"
+              value={selectedSubject?.id || ""}
+              onChange={handleSubjectChange}>
+              <option value="" disabled>
+                Choisir une matière
+              </option>
+              {availableSubjects.map((subject) => (
+                <option key={subject.id} value={subject.id}>
+                  {subject.name}
+                </option>
+              ))}
+            </select>
+
+            {/* Bouton suivant */}
+            <button
+              onClick={() => {
+                const nextSubjectIndex =
+                  availableSubjects.findIndex((s) => s.id === selectedSubject?.id) +
+                  1;
+                if (nextSubjectIndex < availableSubjects.length) {
+                  const nextSubject = availableSubjects[nextSubjectIndex];
+                  setSelectedSubject(nextSubject);
+                  setCurrentCourses(nextSubject.courses || []);
+                }
+              }}
+              disabled={
+                !selectedSubject ||
+                availableSubjects.indexOf(selectedSubject) >=
+                  availableSubjects.length - 1
+              }
+              className={`flex w-48 h-10 mt-2 items-center px-4 py-2 text-white bg-primary rounded-full
+                shadow-md hover:bg-primaryshade focus:bg-primarytint border border-white focus:outline-none
+                ${
+                  !selectedSubject ||
+                  availableSubjects.indexOf(selectedSubject) >=
+                    availableSubjects.length - 1
+                    ? "bg-primaryshade cursor-not-allowed"
+                    : ""
+                }`}>
+              Passer au suivant
+              <img
+                src="/images/right-arrow.svg"
+                alt="Right Arrow"
+                className="ml-2 w-4 h-4"/>
+            </button>
           </div>
-        </DndProvider>
+
+          {groups.length === 0 ? (
+            <div className="flex items-center justify-center w-full">
+              <div className="w-1/2 text-center text-primary mt-10 text-lg font-bold p-2 bg-white rounded-full">
+                Il n'y a pas de groupes, veuillez en ajouter pour consulter le tableau.
+              </div>
+            </div>
+          ) : (
+            <DndProvider backend={HTML5Backend}>
+              <div className="ml-36 rounded-lg overflow-auto max-h-[75vh] min-h-[25rem] max-w-[85vw] -z-10">
+                <div
+                  className="grid"
+                  style={{
+                    gridTemplateColumns: `40px repeat(${groupList.length}, minmax(5rem, 1fr))`,
+                  }}>
+                  <div className="w-10 h-6"></div>
+                  {groupList.map((groupName, colIndex) => (
+                    <div
+                      key={`col-label-${colIndex}`}
+                      className="w-full h-6 bg-gray-200 flex items-center justify-center text-black text-sm font-bold">
+                      {groupName}
+                    </div>
+                  ))}
+                  {Array.from({ length: GRID_ROW_LENGTH }).map((_, rowIndex) => (
+                    <React.Fragment key={`row-${rowIndex}`}>
+                      <div className="h-20 w-10 bg-gray-200 flex items-center justify-center text-black text-sm font-bold">
+                        S{rowIndex + 1}
+                      </div>
+                      {groupList.map((_, colIndex) => {
+                        const positionKey = `${rowIndex}-${colIndex}`;
+                        const cellItems = items[positionKey] || [];
+                        return (
+                          <Node
+                            key={positionKey}
+                            positionKey={positionKey}
+                            items={cellItems}
+                            moveItem={moveItem}
+                          />
+                        );
+                      })}
+                    </React.Fragment>
+                  ))}
+                </div>
+              </div>
+            </DndProvider>
+          )}
+        </>
       )}
     </div>
   );
