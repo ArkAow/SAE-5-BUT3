a097c81fd9dc78473c09f18af044d71a1ab518b9
Tom <tom.charpentier@etu.unilim.fr>
2024-12-09 10:51:17 +0100

    changed teacher controller

--- backend/src/Controller/TeacherController.php
+++ backend/src/Controller/TeacherController.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Teacher;
 use App\Entity\Professor;
 use App\Entity\PartTimeTutor;
 
-class AddTeacherController extends AbstractController
+class TeacherController extends AbstractController
 {
     #[Route('/add/teacher', name: 'app_add_teacher')]
-    public function addTeacher($rowData): JsonResponse
+    public function addTeacher(EntityManagerInterface $entityManager): JsonResponse
     {
         
         return new JsonResponse(['status' => 'success']);
