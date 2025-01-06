650f076e1ccda9bf01da38db528415f4fb77475b
Mathis Krizmanic <mathis.krizmanic@gmail.com>
2025-01-06 10:09:51 +0100

    Correction on migration 708

--- backend/migrations/Version20241120093708.php
+++ backend/migrations/Version20241120093708.php
@@ -323,6 +323,6 @@ final class Version20241120093708 extends AbstractMigration
         $schema->dropTable('user_department');
         $schema->dropTable('archive');
         $schema->dropTable('archive_department');
-        $schema->dropTable('course_formationLevel');   
+        $schema->dropTable('course_formation_level');   
     }
 }
