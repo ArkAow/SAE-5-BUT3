087ff5a8a89e552bed4529a2a9e1668f9ff71ca7
Tom <tom.charpentier@etu.unilim.fr>
2025-01-28 14:59:23 +0100

    Added header

--- app/src/components/statistics/Statistics.js
+++ app/src/components/statistics/Statistics.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../header/header";
 
 const Statistics = () => {
 
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
