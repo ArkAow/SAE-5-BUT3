7ed14fa24f863f446e7accdd6acf43f6b6fdfa3e
Tom <tom.charpentier@etu.unilim.fr>
2025-01-29 10:44:15 +0100

    Implemented the page

--- app/src/components/statistics/Statistics.js
+++ app/src/components/statistics/Statistics.js
@@ -6,6 +6,22 @@ const Statistics = () => {
   return (
     <>
       <Header />
+      <div className="min-h-screen flex flex-col items-center p-6 bg-cover bg-center">
+        <div className="flex flex-row justify-between w-full max-w-[90vw] mt-16">
+          <div className="h-[70vh] w-[65vw] rounded-lg bg-white shadow-lg p-4">
+            Graph et statistiques
+          </div>
+
+          <div className="flex flex-col justify-between items-left ml-6">
+            <div className="h-[25vh] w-[10vw] rounded-lg bg-white shadow-lg p-4">
+              Légende du graph
+            </div>
+            <div className="h-[40vh] w-[20vw] rounded-lg bg-white shadow-lg p-4">
+              graph répartition des heures
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
