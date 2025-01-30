3a0af60bc090914f583e0966d4c66ad59f06012c
lafarge36 <baptiste.lafarge1@etu.unilim.fr>
2025-01-30 09:30:18 +0100

    feature nouvelle page

--- app/src/components/ManageData/ModifyTeachers.js
+++ app/src/components/ManageData/ModifyTeachers.js
@@ -152,82 +152,35 @@ const ModifyTeachers = () => {
       {toast.visible && (
         <Toast message={toast.message} type={toast.type} onClose={() => setToast({ ...toast, visible: false })} />
       )}
-      {/* Navigation */}
-      <div className="absolute flex flex-row items-center top-16 left-10 space-x-2">
-        <div 
-          className="flex flex-row items-center py-4 px-4
-          bg-black bg-opacity-70 text-xl space-x-4 w-fit rounded-lg cursor-pointer"
-          onClick={goToHomePage}>
-          <img 
-              src="/images/home.svg"
-              className="size-8"/>
-        </div>
-        <div 
-          className="flex flex-row items-center py-4 px-4
-          bg-black bg-opacity-70 text-xl space-x-4 w-fit rounded-lg cursor-pointer"
-          onClick={goToManageData}>
-          <img 
-              src="/images/options.svg"
-              className="size-8"/>
-        </div>
+      <div className="flex flex-row items-center mt-16 ml-10 py-1 px-8 text-white bg-black bg-opacity-70 text-xl space-x-4 w-fit rounded-lg">
+        <span onClick={goToHomePage} className="cursor-pointer hover:underline">Page d'accueil /</span>
+        <img src="/images/options.svg" alt="Options Icon" className="w-8 h-8" />
+        <span onClick={goToManageData} className="cursor-pointer hover:underline">Gestion des données /</span>
+        <span>Modifier les enseignants</span>
       </div>
 
       <div className="flex flex-col items-center justify-center flex-1 space-y-5 py-10">
-        <div className="flex flex-row w-[70vw] min-w-80 max-w-[55rem] items-start bg-black bg-opacity-75 p-10 rounded-lg justify-around space-x-10">
-          <form onSubmit={handleAddTeacher} className="flex flex-col space-y-4">
-            <h1 className="text-white text-3xl font-bold mt-1">Ajouter un enseignant</h1>
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              placeholder="Prénom"
-              className="p-2 rounded"
-            />
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              placeholder="Nom"
-              className="p-2 rounded"
-            />
-            <input
-              type="number"
-              min="0"
-              max="40"
-              value={constraint}
-              onChange={(e) => setConstraint(e.target.value)}
-              placeholder="Nombre maximum d'heures / semaines"
-              className="p-2 rounded"
-            />
-            <label className="flex justify-center items-center space-x-2 text-white w-full">
-              <input
-                type="checkbox"
-                checked={isPartTime}
-                onChange={(e) => setIsPartTime(e.target.checked)}
-                className="w-4 h-4 rounded border-gray-300 text-red-500 focus:ring-red-500"
-              />
-              <span>Enseignant à temps partiel</span>
-            </label>
-            <button type="submit" className="btn-default p-2">Ajouter</button>
-          </form>
-
-          <div className="flex flex-col max-h-80 items-center justify-center bg-black bg-opacity-75 p-6 rounded-lg transition-opacity duration-300">
-            {loading ? (
-              <div className="flex flex-col items-center justify-center bg-black bg-opacity-75 p-6 rounded-lg 
-              transition-opacity duration-300 opacity-100 w-60">
-                <div className="spinner"></div>
-                <div className="text-white text-xl font-bold text-center mt-4 max-h-[300px] h-max">Chargement des enseignants...</div>
-              </div>
-            ) : (
-              <div className="text-white text-l font-bold m-1 overflow-y-auto custom-scrollbar-light min-w-44 w-60 max-h-[300px]">
+  <div className="flex flex-row w-[70vw] min-w-80 max-w-[55rem] items-start bg-black bg-opacity-75 p-2 rounded-lg justify-between">
+     
+    
+    {/* Div du milieu qui prend le plus d'espace */}
+    <div className="flex flex-col flex-grow  p-6 rounded-lg transition-opacity duration-300">
+      {loading ? (
+        <div className="flex flex-col items-center justify-center  p-6 rounded-lg transition-opacity duration-300 opacity-100 w-full">
+          <div className="spinner"></div>
+          <div className="text-white text-xl font-bold text-center mt-4 max-h-[300px] h-max">Chargement des enseignants...</div>
+        </div>
+      ) : (
+        <div className="text-white text-l font-bold m-1 overflow-y-auto custom-scrollbar-light min-w-44 w-full max-h-[300px]">
+           <span className="text-white text-xl font-bold pb-3 block"> Enseignants</span>
                 {teachers.length === 0 ? (
                   <span>Il n'y a pas d'enseignants.</span>
                 ) : (
                   <ul className="space-y-4">
                     {teachers.map((teacher) => (
                       <li key={teacher.id} className="flex justify-between items-center bg-white rounded-lg p-2">
-                        <span className="text-base text-black max-w-[70%]">{teacher.code}
-                          <span className="font-normal text-sm"> ({teacher.firstName} {teacher.lastName.toUpperCase()})</span>
+                        <span className="text-base text-black max-w-[80%]">{teacher.code}
+                          <span className="font-normal text-sm"> maximum {teacher.time_constraints}h / semaines</span>
                         </span>
                         <div>
                           <button
