386a9506734a62c504fba9c82371abea665e2c6e
Axel Pignol <mapou03@icloud.com>
2025-02-01 13:46:16 +0100

    add the route for the connexion user->department

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Department;
 
 class UserController extends AbstractController
 {
@@ -58,5 +59,90 @@ class UserController extends AbstractController
                 'role' => $user->getRole(),
             ]
         ], Response::HTTP_OK);
+    }
+
+    #[Route("/users/{id}/department", name:"get_department_for_a_user", methods:['GET'])]
+    public function getDepartmentForAUser(EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->find($id);
+
+        if (!$user)
+        {
+            return new JsonResponse(['error' => "L\'utilisateur n\'existe pas ou n\' pas été trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        $departments = $user->getDepartments();
+        $data = [];
+
+        foreach ($departments as $department)
+        {
+            $data[] = [
+                'id' => $department->getId(),
+                'name' => $department->getName()
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    #[Route("user/{id}/department/{departmentId}", name:"add_department_to_user", methods:['POST'])]
+    public function addDepartmentForAUser(EntityManagerInterface $entityManager, int $id, int $departmentId): JsonResponse
+    {
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->find($id);
+
+        if (!$user)
+        {
+            return new JsonResponse(['error' => "L\'utilisateur n\'existe pas ou n\' pas été trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        $departmentRepository = $entityManager->getRepository(Department::class);
+        $department = $departmentRepository->find($departmentId);
+
+        if (!$department)
+        {
+            return new JsonResponse(['error' => "Le département n\'existe pas ou n\' pas été trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($user->getDepartments()->contains($department))
+        {
+            return new JsonResponse(['error' => "Le département est déjà associé à l\'utilisateur"], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->addDepartment($department);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => "Département ajouté à l\'utilisateur avec succès"], Response::HTTP_OK);
+    }
+
+    #[Route("user/{id}/department/{departmentId}", name:"modify_department_from_user", methods:['PUT'])]
+    public function modifyDepartmentForAUser(EntityManagerInterface $entityManager, int $id, int $departmentId): JsonResponse
+    {
+        $userRepository = $entityManager->getRepository(User::class);
+        $user = $userRepository->find($id);
+
+        if (!$user)
+        {
+            return new JsonResponse(['error' => "L\'utilisateur n\'existe pas ou n\' pas été trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        $departmentRepository = $entityManager->getRepository(Department::class);
+        $department = $departmentRepository->find($departmentId);
+
+        if (!$department)
+        {
+            return new JsonResponse(['error' => "Le département n\'existe pas ou n\' pas été trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        if (!$user->getDepartments()->contains($department))
+        {
+            return new JsonResponse(['error' => "Le département n\'est pas associé à l\'utilisateur"], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->removeDepartment($department);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => "Département retiré de l\'utilisateur avec succès"], Response::HTTP_OK);
     }
 }
